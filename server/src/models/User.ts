b440085353ac4c1d274901b6c1804b717887350d
kurlee710 <josephatamaniuk@gmail.com>
2024-11-14 12:02:47 -0500

    defined the User schema

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -10,3 +10,11 @@ interface IUser extends Document {
   password: string;
   comparePassword: (password: string) => Promise<boolean>;
 }
+
+// Define the User schema
+const UserSchema = new Schema<IUser>({
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  username: { type: String, required: true, unique: true, minlength: 8 },
+  password: { type: String, required: true, minlength: 8 },
+});

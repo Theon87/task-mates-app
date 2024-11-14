f67686081b38d922755e51018476dd862e4bca96
kurlee710 <josephatamaniuk@gmail.com>
2024-11-14 14:03:09 -0500

    created Task schema

--- server/src/models/Task.ts
+++ server/src/models/Task.ts
@@ -3,7 +3,7 @@ import { Schema, model, type Document } from "mongoose";
 // Define an interface for the Task document
 interface ITask extends Document {
   id: number;
-  create: string;
+  creator: string;
   assignees: string[];
   descprtion: Text;
   status: boolean;
@@ -11,3 +11,14 @@ interface ITask extends Document {
   due_date: Date;
   date_completed: Date;
 }
+
+// Define the Task schema
+const taskSchema = new Schema<ITask>({
+  creator: { type: String, required: true },
+  assignees: { type: [String], required: true },
+  description: { type: Text, required: true },
+  status: { type: Boolean, required: true },
+  created_at: { type: Date, required: true },
+  due_date: { type: Date, required: true },
+  date_completed: { type: Date, required: true },
+});

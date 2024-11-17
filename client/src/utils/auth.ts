6863864e64711c63d1d2bb415268993305ef295d
Theon87 <theonfrancis17@gmail.com>
2024-11-17 15:17:30 -0500

    add isTokenExpired method to AuthService

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,7 +1,30 @@
-// import { type JwtPayload, jwtDecode } from 'jwt-decode';
+import { type JwtPayload, jwtDecode } from 'jwt-decode';
+
+interface ExtendedJwt extends JwtPayload {
+    data: {
+        username: string;
+        email: string;
+        _id: string;
+    }
+};
 
 class AuthService {
-    
+    // check if the user is logged in by checking if the token is expired
+    isTokenExpired(token: string): boolean {
+        try {
+            const decoded = jwtDecode<JwtPayload>(token);
+
+            if (decoded?.exp && decoded?.exp < Date.now() / 1000) {
+                return true;
+            }
+            return false;
+            
+        } catch (err) {
+            return false;
+        }
+    }
+
+
 }
 
 export default new AuthService();

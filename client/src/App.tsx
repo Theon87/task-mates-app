1fec843ae3f5eb611f21bad76448ab860e6c40a3
kurlee710 <josephatamaniuk@gmail.com>
2024-11-17 15:35:59 -0500

    Imported setContext

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,17 +6,37 @@ import {
   createHttpLink,
 } from "@apollo/client";
 import { Outlet } from "react-router-dom";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <div className="card">
         <h1> HELLO WORLD!</h1>
         <div>
           <Outlet />
         </div>
       </div>
-    </>
+    </ApolloProvider>
   );
 }
 

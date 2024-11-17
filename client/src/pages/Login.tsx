8181b459f357dcd102f5aab753906a4b6018f20c
Theon87 <theonfrancis17@gmail.com>
2024-11-16 19:30:53 -0500

    hardcoded login page return

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,1 +1,11 @@
-import { useMutation } from '@apollo/client';
+// import { useMutation } from '@apollo/client';
+
+const Login = () => {
+    return (
+        <div>
+        <h1>Login</h1>
+        </div>
+    );
+};
+
+export default Login;

0d40c9fb95329235aa36b5c97dedc0e94432910b
Theon87 <theonfrancis17@gmail.com>
2024-11-17 17:06:51 -0500

    add mutation to add user

--- client/src/utils/mutations.ts
+++ client/src/utils/mutations.ts
@@ -1,1 +1,16 @@
-// import { gql } from '@apollo/client';
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+mutation Signup($input: UserInput!) {
+  addUser(input: $input) {
+    token
+    user {
+      username
+      first_name
+      last_name
+      email
+      _id
+    }
+  }
+}
+`;

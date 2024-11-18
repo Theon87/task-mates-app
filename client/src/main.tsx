c77856c4e3e96522617d2e1066cdd3b380c02641
Theon87 <theonfrancis17@gmail.com>
2024-11-18 13:59:58 -0500

    make login page the landing page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -17,11 +17,11 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <TaskList />,
+        element: <Login />,
       },
       {
-        path: "/login",
-        element: <Login />,
+        path: "/tasks",
+        element: <TaskList />,
       },
       {
         path: "/signup",

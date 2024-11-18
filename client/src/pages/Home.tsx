8859793e9ec2a3f20635d5fd94c3dbde165325a4
Theon87 <theonfrancis17@gmail.com>
2024-11-18 18:47:29 -0500

    add home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,16 +1,45 @@
-import { useState, useEffect, useLayoutEffect } from "react";
+import { useState, useLayoutEffect } from "react";
 import { Link } from "react-router-dom";
 
-import ErrorPage from "./Error";
 import TaskList from "../components/TaskList";
 import auth from "../utils/auth";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
+
+    const [loginCheck, setLoginCheck] = useState(false);
+
+    useLayoutEffect(() => {
+        checkLogin();
+    }, []);
+
+    const checkLogin = () => {
+        if (auth.isLoggedIn()) {
+            setLoginCheck(true);
+        } else {
+            setLoginCheck(false);
+        }
+    };
+
     return (
         <>
-            <h1>Home</h1>
+            {
+                !loginCheck ? (
+                    <div>
+                        <h1>Welcome to Task Manager</h1>
+                        <p>Task Manager is a simple app that allows you to manage your tasks.</p>
+                        <p>Click <Link to="/login">here</Link> to login or <Link to="/signup">here</Link> to sign up.</p>
+                    </div>
+                ) : (
+                    <div>
+                        <Navbar />
+                        <TaskList />
+                    </div>
+  
+                )
+            }
         </>
-    )
+    );
 };
 
 export default Home;

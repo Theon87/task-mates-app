d1d1bcc24e6ccde163256b59be5a6b478f85d7f8
jerodj23 <rssjero@gmail.com>
2024-11-18 11:33:51 -0500

    added state to hold current value of the task input

--- client/src/components/AddTask.tsx
+++ client/src/components/AddTask.tsx
@@ -1,4 +1,10 @@
 import React, { useState}  from 'react';
+import { addSyntheticTrailingComment } from 'typescript';
 
+interface AddTaskProps {
+    onAdd: (task:string) => void; // function to handle new task addition
+}
 
-
+const AddTask: React.FC<AddTaskProps> = ({ onAdd }) => {
+    const [task, setTask] = useState("");
+}

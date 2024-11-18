2b67f84fcd9d9241d52e0ece7c4660bb38787f49
jerodj23 <rssjero@gmail.com>
2024-11-18 11:46:32 -0500

    addtask"

--- client/src/components/AddTask.tsx
+++ client/src/components/AddTask.tsx
@@ -1,31 +1,11 @@
-import React, { useState}  from 'react';
+// import { useMutation } from '@apollo/client';
 
-interface AddTaskProps {
-    onAdd: (task:string) => void; // function to handle new task addition
-}
-
-const AddTask: React.FC<AddTaskProps> = ({ onAdd }) => {
-    const [task, setTask] = useState("");
-}
-
-const handleSubmit = (event: React.FromEvent) => {
-    event.preventDefault();
-    if (task.trim()) {
-        onAdd(task);
-        setTask("");
-    }
-};
-return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={task}
-        onChange={(e) => setTask(e.target.value)} // Update state on input change
-        placeholder="Enter new task"
-      />
-      <button type="submit">Add Task</button>
-    </form>
-  );
+const AddTask = () => {
+    return (
+        <div>
+        <h1>AddTask</h1>
+        </div>
+    );
 };
 
 export default AddTask;

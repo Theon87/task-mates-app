6b0a0599e8b99f503931936af77f6cf6a7965881
kurlee710 <josephatamaniuk@gmail.com>
2024-11-18 11:45:16 -0500

    changed function to handle login

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,11 +1,113 @@
-// import { useMutation } from '@apollo/client';
+import { useState, type FormEvent, type ChangeEvent } from "react";
+// import { Link } from "react-router-dom";
+
+import { useMutation } from "@apollo/client";
+
+import Auth from "../utils/auth";
+import { LOGIN_USER } from "../utils/mutations";
 
 const Login = () => {
-    return (
-        <div>
-        <h1>Login</h1>
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [login] = useMutation(LOGIN_USER);
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    console.log(formState);
+
+    try {
+      const { data } = await login({
+        variables: { input: { ...formState } },
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  return (
+    <div className="ui container" style={{ marginTop: "50px" }}>
+      <div className="ui centered grid">
+        <div className="six wide column">
+          <h2 className="ui teal header">
+            <i className="signup icon"></i>
+            <div className="content">Sign Up</div>
+          </h2>
+          <form
+            className="ui form"
+            id="signup-form"
+            onSubmit={handleFormSubmit}
+          >
+            <div className="field">
+              <label>First Name</label>
+              <input
+                type="text"
+                name="first_name"
+                placeholder="First Name"
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div className="field">
+              <label>Last Name</label>
+              <input
+                type="text"
+                name="last_name"
+                placeholder="Last Name"
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div className="field">
+              <label>Username</label>
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div className="field">
+              <label>Email</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <div className="field">
+              <label>Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <button className="ui teal button" type="submit">
+              Sign Up
+            </button>
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;

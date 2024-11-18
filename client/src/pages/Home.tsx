7b48e3b21f10340b6a58429b53c4ff8aed76cd74
Theon87 <theonfrancis17@gmail.com>
2024-11-18 17:59:26 -0500

    add home page and move tasklist page to components

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect, useLayoutEffect } from "react";
+import { Link } from "react-router-dom";
+
+import ErrorPage from "./Error";
+import TaskList from "../components/TaskList";
+import auth from "../utils/auth";
+
+const Home = () => {
+    return (
+        <>
+            <h1>Home</h1>
+        </>
+    )
+};

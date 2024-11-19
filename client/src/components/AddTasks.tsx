79ef9a1c48b29ccfa3bc313d4b13d06decde1000
kurlee710 <josephatamaniuk@gmail.com>
2024-11-19 10:31:57 -0500

    added onClick event for button in AddTasks component

---
+++ client/src/components/AddTasks.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Button, Icon } from "semantic-ui-react";
+
+interface AddTaskButtonProps {
+  onClick: () => void; // Callback function when button is clicked
+}

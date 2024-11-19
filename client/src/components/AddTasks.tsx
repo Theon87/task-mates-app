de180ac18b382daa043f1ebf5de70730128f1586
kurlee710 <josephatamaniuk@gmail.com>
2024-11-19 10:33:06 -0500

    added HTML return for AddTasks component

--- client/src/components/AddTasks.tsx
+++ client/src/components/AddTasks.tsx
@@ -4,3 +4,20 @@ import { Button, Icon } from "semantic-ui-react";
 interface AddTaskButtonProps {
   onClick: () => void; // Callback function when button is clicked
 }
+
+const AddTaskButton: React.FC<AddTaskButtonProps> = ({ onClick }) => {
+  return (
+    <Button
+      color="blue" // Choose a color for the button
+      icon
+      labelPosition="left"
+      onClick={onClick}
+      style={{ margin: "1rem 0" }} // Add some spacing around the button
+    >
+      <Icon name="plus" />
+      Add Task
+    </Button>
+  );
+};
+
+export default AddTaskButton;

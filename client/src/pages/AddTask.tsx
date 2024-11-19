f8c830457c48e86e99d15da0f8c7fe93cebfe7dc
Paola Nicole Ayala <paolanayala@gmail.com>
2024-11-19 12:29:46 -0500

    amended form to match site UI

--- client/src/pages/AddTask.tsx
+++ client/src/pages/AddTask.tsx
@@ -16,13 +16,27 @@ const TaskList = () => {
 
     useEffect (() => {
         if (data && data.tasks) {
-            const completedTasks = data.tasks.filter((task) => task.status === true);
+            const completedTasks = data.tasks.filter((task: { status: boolean; }) => task.status === true);
             setTasks(completedTasks);
         }
 }, [data]);
 
-if (loading) return <p>Loading...</p>;
-if (error) return <p>Error loading tasks...</p>;
+const [addTask, { error }] = useMutation
+(ADD_TASK, {
+    refetchQueries: [{ 
+        query: GET_TASKS, variables: { task_name: '' } }],
+});
+
+// const handleFormSubmit = async (event: FormEvent) => {
+//     event.preventDefault();
+
+    function handleFormSubmit(_event: FormEvent<HTMLFormElement>): void {
+        throw new Error('Function not implemented.');
+    }
+
+    function handleChange(_event: ChangeEvent<HTMLInputElement>): void {
+        throw new Error('Function not implemented.');
+    }
 
 // const tasks = () => {
 //     const [ formState, setFormState ] = useState({
@@ -35,56 +49,79 @@ if (error) return <p>Error loading tasks...</p>;
 
 
 // const AddTask: React.FC = () => {
-//     const [task_name, setTaskName] = useState('');
-//     const [description, setDescription] = useState('');
+    const [task_name, setTaskName] = useState('');
+    const [description, setDescription] = useState('');
 //     const [due_date, setDueDate] = useState('');
     
 //     const [addTask, { error }] = useMutation(ADD_TASK);
 //     const navigate = useNavigate();
 
-//     const handleSubmit = async (e: React.FormEvent) => {
-//         e.preventDefault();
-//         try {
-//             await addTask({ 
-//                 variables: { 
-//                     task_name, description } });
-//             setTaskName('');
-//             setDescription('');
-//         } 
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            await addTask({ 
+                variables: { 
+                    task_name, description } });
+            setTaskName('');
+            setDescription('');
+        } 
 // //Redirect to task list page after the successful addition of a task
 //         navigate('/');
 //     },   catch (err) {
 //     console.error('Error adding task:', err);
 // }
 // };
+if (loading) return <p>Loading...</p>;
+if (error) return <p>Error loading tasks...</p>;
 
 
-    return (
-     <div>
-        <h1>Add Task</h1>
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="task_name">Task Title:</label>
-                <input
-                    type="text"
-                    id="task_name"
-                    value={task_name}
-                    onChange={(e) => setTaskName(e.target.value)}
-                    />
-            </div>
-            <div>
-                <label htmlFor="description">Description:</label>
-                <textarea
-                    id="description"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    />
-            </div>
-            <button type="submit">Add Task</button> 
-            {/* updates database with new task */}
-        </form>
-        {error && <p>Error: {error.message}</p>}
-     </div>
+
+return (
+    <div className="ui container" style={{ marginTop: "50px" }}>
+      <div className="ui centered grid">
+        <div className="eleven wide column">
+          <h2 className="ui teal header">
+            <i className="signup icon"></i>
+            <div className="content">Add Task</div>
+                    </h2>
+                    <form
+                        className="ui form"
+                        id="signup-form"
+                        onSubmit={handleSubmit}
+                    >
+                        <div className="field">
+                            <label>Task Name</label>
+                            <input
+                                type="text"
+                                name="task_name"
+                                placeholder="Task Name"
+                                required
+                                onChange={(e) => setTaskName(e.target.value)}
+                            />
+                        </div>
+                        <div className="field">
+                            <label>Last Name</label>
+                            <input
+                                type="text"
+                                name="setDescription"
+                                placeholder="Task Description"
+                                required
+                                onChange={(e) => setDescription(e.target.value)}
+                            />
+                        </div>
+                        <div className="field">
+                            <label>Due Date</label>
+                            <input
+                                type="date"
+                                name="due_date"
+                                required
+                                onChange={(e) => setDueDate(e.target.value)}
+                            />
+                        </div>
+            </form>
+        </div>
+    </div>
+</div>
     );
 };
 

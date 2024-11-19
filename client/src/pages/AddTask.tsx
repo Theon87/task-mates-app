135348ae437c72b8250b97b1d2166b83f19d6807
Paola Nicole Ayala <paolanayala@gmail.com>
2024-11-19 12:07:51 -0500

    amended queries while working on addTask functionality

--- client/src/pages/AddTask.tsx
+++ client/src/pages/AddTask.tsx
@@ -1,22 +1,63 @@
-import { useMutation } from '@apollo/client';
-import React, { useState } from 'react';
-import { ADD_TASK } from '../utils/mutations';
+import { useMutation, useQuery } from '@apollo/client';
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import { Container, Header, Icon, List, Segment } from 'semantic-ui-react';
+import { GET_TASKS,  ADD_TASK } from '../utils/queries';
 
-const AddTask: React.FC = () => {
-    const [task_name, setTaskName] = useState('');
-    const [description, setDescription] = useState('');
-    const [addTask, { error }] = useMutation(ADD_TASK);
+import { useNavigate } from 'react-router-dom';
 
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        try {
-            await addTask({ variables: { task_name, description } });
-            setTaskName('');
-            setDescription('');
-        } catch (err) {
-            console.error(err);
+//import AddTaskButton from '../components/AddTaskButton';
+
+const TaskList = () => {
+    const { loading, error, data } = useQuery(GET_TASKS,{
+        variables: { task_name: '' },
+    });
+
+    const [tasks, setTasks] = useState([]);
+
+    useEffect (() => {
+        if (data && data.tasks) {
+            const completedTasks = data.tasks.filter((task) => task.status === true);
+            setTasks(completedTasks);
         }
-    };
+}, [data]);
+
+if (loading) return <p>Loading...</p>;
+if (error) return <p>Error loading tasks...</p>;
+
+// const tasks = () => {
+//     const [ formState, setFormState ] = useState({
+//         task_name: '',
+//         description: '',
+//         due_date: '',
+//     });
+// }
+
+
+
+// const AddTask: React.FC = () => {
+//     const [task_name, setTaskName] = useState('');
+//     const [description, setDescription] = useState('');
+//     const [due_date, setDueDate] = useState('');
+    
+//     const [addTask, { error }] = useMutation(ADD_TASK);
+//     const navigate = useNavigate();
+
+//     const handleSubmit = async (e: React.FormEvent) => {
+//         e.preventDefault();
+//         try {
+//             await addTask({ 
+//                 variables: { 
+//                     task_name, description } });
+//             setTaskName('');
+//             setDescription('');
+//         } 
+// //Redirect to task list page after the successful addition of a task
+//         navigate('/');
+//     },   catch (err) {
+//     console.error('Error adding task:', err);
+// }
+// };
+
 
     return (
      <div>
@@ -39,7 +80,8 @@ const AddTask: React.FC = () => {
                     onChange={(e) => setDescription(e.target.value)}
                     />
             </div>
-            <button type="submit">Add Task</button>
+            <button type="submit">Add Task</button> 
+            {/* updates database with new task */}
         </form>
         {error && <p>Error: {error.message}</p>}
      </div>
